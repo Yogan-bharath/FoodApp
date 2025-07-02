f56b1b6c3e9ebab89373439132ce2c97937f30d5
Yogan-bharath <yoganbharath@gmail.com>
2025-07-02 15:22:59 +0530

    Update App.jsx

--- adminfrontend/src/App.jsx
+++ adminfrontend/src/App.jsx
@@ -8,7 +8,7 @@ import List from './screens/List/List'
 import Orders from './screens/Orders/Orders'
 import {ToastContainer} from 'react-toastify'
 const App = () => {
-  const url = `http://localhost:4000`
+  const url = `https://foodapp-backend-k1ek.onrender.com`
 
   return (
     <>
